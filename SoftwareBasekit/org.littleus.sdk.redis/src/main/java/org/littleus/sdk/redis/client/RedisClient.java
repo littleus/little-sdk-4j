f20046e8a781eb6d79e4f864a621e97a9218554e
hyzhangit <1043181710@qq.com>
2022-08-31 01:30:13 +0800

    add parent module

--- SoftwareBasekit/org.littleus.sdk.redis/src/main/java/org/littleus/sdk/redis/client/RedisClient.java
+++ SoftwareBasekit/org.littleus.sdk.redis/src/main/java/org/littleus/sdk/redis/client/RedisClient.java
@@ -3,18 +3,21 @@ package org.littleus.sdk.redis.client;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
 import redis.clients.jedis.params.ScanParams;
+import redis.clients.jedis.resps.ScanResult;
 
 /**
  * @author hyzhangj
  */
 public class RedisClient {
     public Jedis getJedis() {
-        Jedis jedis = null;
-        jedis.scan("", ScanParams);
-        jedis.hscan()
-
         JedisPool jedisPool = null;
 
-        return null;
+        try (Jedis jedis = jedisPool.getResource()){
+            ScanParams scanParams = new ScanParams().match("*abc*").count(10000);
+            ScanResult<String> scanResult = jedis.scan("0", scanParams);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }
