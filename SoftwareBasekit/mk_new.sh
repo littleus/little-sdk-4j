baf905c61309ed0120400e12bf4c3a9c3d1db26c
hyzhangit <1043181710@qq.com>
2023-03-18 23:41:34 +0800

    rename project

--- SoftwareBasekit/mk_new.sh
+++ SoftwareBasekit/mk_new.sh
@@ -9,8 +9,8 @@ function main() {
   fi
 
   cp -R ./org.littleus.sdk.aaa ./org.littleus.sdk."$name"
-  sed -i "s/littleus-sdk-aaa/littleus-sdk-${name}/g" ./org.littleus.sdk."$name"/pom.xml
-  mv ./org.littleus.sdk."$name"/doc/littleus-sdk-aaa.md ./org.littleus.sdk."$name"/doc/littleus-sdk-"$name".md
+  sed -i "s/little-sdk-aaa/little-sdk-${name}/g" ./org.littleus.sdk."$name"/pom.xml
+  mv ./org.littleus.sdk."$name"/doc/little-sdk-aaa.md ./org.littleus.sdk."$name"/doc/little-sdk-"$name".md
 
 }
 
